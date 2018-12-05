8f88db15e2a57ee9d4a9a2f69a5ce4d93162aff1
Zach Leach <zachleach@techoverco.com>
2018-12-05 10:26:58 -0700

    Added test_sal_can_learn_jokes.

--- lib/user.rb
+++ lib/user.rb
@@ -1,14 +1,24 @@
+
+
 class User
 
-  attr_reader :name, :jokes
+  attr_reader :name, :jokes, :tell
 
   def initialize(name)
     @name = name
     @jokes = []
+    @tell
+    @joke_by_id
   end
 
   def learn(jokes)
-    @jokes  <<  jokes
+    @jokes << jokes
   end
 
+  def tell(name, jokes)
+  end
+
+  def joke_by_id(jokes)
+    return @jokes
+  end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -9,6 +9,7 @@ class UserTest < Minitest::Test
 
   def setup
     @sal = User.new("Sal")
+    @ali = User.new("Ali")
   end
 
   def test_it_exists
@@ -25,5 +26,32 @@ class UserTest < Minitest::Test
     @sal.learn(joke_1)
     @sal.learn(joke_2)
     @sal.jokes
+    assert_equal 2, @sal.jokes.count
   end
+
+  def test_sal_can_tell_ali_joke
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    @sal.tell(@ali, joke_1)
+    @sal.tell(@ali, joke_2)
+  end
+
+  def test_joke_by_id
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+    assert_equal 2, @sal.joke_by_id(1)
+  end
+
+    # pry(main)> sal.tell(ali, joke_1)
+    #
+    # pry(main)> sal.tell(ali, joke_2)
+    #
+    # pry(main)> ali.jokes
+    # # => [#<Joke:0x00007fb71da169f0...>, #<Joke:0x00007fb71d8e0bd0...>]
+    #
+    # pry(main)> ali.joke_by_id(1)
+    # # => #<Joke:0x00007fb71da169f0...>
+    #
+    # pry(main)> ali.joke_by_id(2)
+    # # => #<Joke:0x00007fb71d8e0bd0...>
 end
